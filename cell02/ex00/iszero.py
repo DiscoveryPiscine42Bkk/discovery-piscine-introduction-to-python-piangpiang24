fdfd25d97cf7ea21619341cd035b6b57bc2c1a09
piangpiang24 <piangfa2550@gmail.com>
2025-06-17 21:27:17 +0700

    Update iszero.py

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python3
-number = input("Enter a number:n ")
-number = int(number)
-if number == 0:
-print("This number is equal to zero.")
+value = int(input())
+if value == 0:
+  print("This number is equal to zero")
 else:
-print("This number is differrent feom zero.")
+  print("This number is different from zero")
