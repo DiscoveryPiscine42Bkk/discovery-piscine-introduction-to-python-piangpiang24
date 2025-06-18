3c5085858e4898a7c65daafeb84d45af92369e0d
piangpiang24 <piangfa2550@gmail.com>
2025-06-18 14:36:37 +0700

    Update play_with_arrays.py

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,1 +1,9 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+    print(original_array)
 
+    # กรองค่า >= 8 แล้วบวก 2
+    transformed = [x + 2 for x in original_array if x >= 8]
+
+    # แปลงเป็นเซตเพื่อลบค่าซ้ำ
+    result_set = set(transformed)
+    print(result_set)
