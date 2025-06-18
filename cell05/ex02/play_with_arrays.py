957fdf3ec54343fd99040803e70e6c40a3239022
piangpiang24 <piangfa2550@gmail.com>
2025-06-18 14:07:20 +0700

    Update play_with_arrays.py

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,7 +1,6 @@
 original_array = [2, 8, 9, 48, 8, 22, -12, 2]
-print(original_array)
+    print(original_array)
 
-sub_array = original_array[1:6]
-
-new_array = [x + 2 for x in sub_array]
-print(new_array)
+    # กรองค่า >= 8 แล้วบวก 2
+    new_array = [x + 2 for x in original_array if x >= 8]
+    print(new_array)
